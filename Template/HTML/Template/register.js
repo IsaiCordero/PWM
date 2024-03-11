80a1ef435535abe33b3b7a856b034f525b95abf8
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-03-11 13:33:37 +0000

    register javascript

---
+++ Template/HTML/Template/register.js
@@ -0,0 +1,11 @@
+function register(){
+    const name = document.getElementById("name").value;
+    const lastName = document.getElementById("lastName").value;
+    const birthday = document.getElementById("birthday").value;
+    const gender = document.getElementById("gender").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    alert('Register completed');
+    window.location.href = 'https://www.google.com';
+}
