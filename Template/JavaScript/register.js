a2f317d091238fdc8f7000fe41f4035678eacc76
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-03-25 13:19:26 +0000

    register.js and register.html edited

--- Template/JavaScript/register.js
+++ Template/JavaScript/register.js
@@ -1,27 +1,84 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    const registerButton = document.querySelector(".register-button");
+    if(registerButton){
+        registerButton.addEventListener("click", function(event){
+            event.preventDefault();
+            validRegister();
+        });
+    }
+    else{
+        alert("Not function");
+    }
+})
 function validRegister(){
-    const name = document.getElementById("name").value;
+    const userName = $("#userName").val().trim();
+    console.log(userName)
+    const firstName = document.getElementById("name").value;
     const lastName = document.getElementById("lastName").value;
-    const birthday = document.getElementById("birthday").value;
+    const secondSurname = document.getElementById("SecondSurname").value;
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const birthdate = document.getElementById("birthday").value;
     const gender = document.getElementById("gender").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("registerPassword").value;
 
-
-    register();
-    return false;
-
-}
-
-function register() {
-    alert("Register completed")
-    window.location.href = 'login.html';
+    let userInformation = {
+        "username": userName,
+        "password": password,
+        "email": email,
+        "name": firstName,
+        "surname1": lastName,
+        "surname2": secondSurname,
+        "birthdate": birthdate,
+        "height(cm)": height,
+        "weight(kg)": weight,
+        "gender": gender
+    }
+    alert(JSON.stringify(userInformation));
+    $.ajax("http://localhost:3000/users",{
+        data: userInformation,
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (response) {
+            alert(response);
+            window.location.href = "../Template/Login.html"
+        },
+        error: function (xhr, status, error) {
+            console.error("Error registering user:", error);
+        }
+    });
 
 }
 function validInformation(){
     if(document.getElementById("personalForm").checkValidity()){
-        $('#registerTabs a[href="#account"]').tab('show');
+        document.getElementById('personal-tab').classList.remove('active');
+        document.getElementById('account-tab').classList.add('active');
+        document.getElementById("personal").classList.remove("show", "active");
+        document.getElementById("account").classList.add("show", "active");
+
     } else{
         document.getElementById("personalForm").reportValidity();
     }
+}
+
+function validPassword(password) {
+    const passwordRegex = '/^(?=.*[A-Z])(?=.*[0-9])(?=.*[-_!@#?¿!¡\$%\^&\*]).{6,}$/';
+    if (passwordRegex.test(password) === false) {
+        alert("Password must contain at least one uppercase letter, one number, or one special character");
+        return false;
+    }
+    // Si todos los campos están llenos, redirigir al usuario a la página de inicio de sesión
+    window.location.href = "../HTML/Template/Login.html";
+    return true;
+}
+
+function goBack() {
+    document.getElementById('account-tab').classList.remove('active');
+    document.getElementById('personal').classList.add('active');
+    document.getElementById("account").classList.remove("show", "active");
+    document.getElementById("personal").classList.add("show", "active");
 }
 
